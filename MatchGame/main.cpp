26596a5cae430a9dd59fcd27fa975ff100b36f9b
frank-yuan <felk2005@gmail.com>
2016-04-25 09:34:11 +0800

    Initial Commit

---
+++ MatchGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MatchGame
+//
+//  Created by Xuan Yuan (Frank) on 4/25/16.
+//  Copyright © 2016 frank-yuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
