a9a45459d7e0e0c6e18508d5f6783567bade9451
frank-yuan <felk2005@gmail.com>
2016-07-11 16:39:27 +0800

    Load texture with right pixel format

--- MatchGame/Engine/Texture.cpp
+++ MatchGame/Engine/Texture.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_surface.h>
 #include <SDL2_image/SDL_image.h>
 #include <OpenGL/glu.h>
 #include "Texture.hpp"
@@ -35,10 +36,32 @@ namespace XYGame
         glGenTextures(1, mTextureId.get());
         glBindTexture(GL_TEXTURE_2D, *mTextureId);
         int mode;
+        
+        
         switch (mSurface->format->BytesPerPixel) {
             case 4:
+            {
                 mode = GL_RGBA;
+                
+#if __LITTLE_ENDIAN__
+                // For Mac, the pixel format we loaded is not RGBA
+                SDL_PixelFormat pf(*mSurface->format);
+                pf.palette = 0;
+                pf.BitsPerPixel = 32;
+                pf.BytesPerPixel = 4;
+                pf.Rshift = pf.Rloss = pf.Gloss = pf.Bloss = pf.Aloss = 0;
+                pf.Rmask = 0x000000ff;
+                pf.Gshift = 8;
+                pf.Gmask = 0x0000ff00;
+                pf.Bshift = 16;
+                pf.Bmask = 0x00ff0000;
+                pf.Ashift = 24;
+                pf.Amask = 0xff000000;
+                
+                mSurface = std::unique_ptr<SDL_Surface, void(*)(SDL_Surface*)>(SDL_ConvertSurface(mSurface.get(), &pf, SDL_SWSURFACE), SDL_FreeSurface);
+#endif
                 break;
+            }
             case 3:
                 mode = GL_RGB;
                 break;
